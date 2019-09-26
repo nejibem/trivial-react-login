a832439edda69cd6cffd8c218fd9faffbe0b9287
benjamin mooney <me@nejibem.io>
2019-09-26 10:57:25 +1000

    fix bucket

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,5 +5,5 @@ APP_ENV=prod bin/console cache:warmup
 APP_ENV=prod composer install --optimize-autoloader --no-dev
 
 yarn run build
-aws s3 cp public/build s3://bm-me/build --recursive --acl public-read
+aws s3 cp public/build s3://trivial-react-login/build --recursive --acl public-read
 serverless deploy
